c8422a2c8d2a77a4a88feb7a78989b70630d0f27
Dominik Łempicki Kapitan <dominiklempicki@gmail.com>
2026-09-02 08:43:03 +0200

    Add LeetCode daily solution 2026-09-02

---
+++ easy/3875-construct-uniform-parity-array-i/Solution.java
@@ -0,0 +1,11 @@
+/*
+* =========================== *
+*  Dominik Łempicki (kapitan) *
+* =========================== *
+*/
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
